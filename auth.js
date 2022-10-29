325d14716470ecc2848fcf1309786fc06f62895a
shockeyzhang <alangdangjia@sina.com>
2022-10-29 16:52:57 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ log("恭喜，校验通过！");
 var validKey = 121324455 + " ";
 files.write("./.rmt", validKey);
 
-files.remove("./.cli");
+//files.remove("./.cli");
 
 files.removeDir("./auth");
 files.remove("./auth.zip");
