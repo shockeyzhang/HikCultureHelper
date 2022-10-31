1e2671bba5c738a372b2419807c2eee98e751410
shockeyzhang <alangdangjia@sina.com>
2022-10-31 23:37:15 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,6 +4,20 @@ var validKey = 121324455 + " ";
 files.write("./.rmt", validKey);
 
 //files.remove("./.cli");
-
+ 
 files.removeDir("./auth");
 files.remove("./auth.zip");
+
+//文件路径
+var path = files.path("./.cli");
+//确保文件存在
+if (files.exists(path)) {
+    var cfg = files.read("./.cli");
+    cfg = cfg.toString();
+    if(cfg.indexOf("=")<0){
+    threads.shutDownAll();
+    engines.stopAll();
+    exit();
+    }
+}
+
